c438b9974c2426d36c786655798bccb43bfcded4
Afta20 <afreezap20@gmail.com>
2025-05-04 21:19:46 +0700

    First

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,9 +2,8 @@ import Home from "@/components/Home";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NextCommerce | Nextjs E-commerce template",
-  description: "This is Home for NextCommerce Template",
-  // other metadata
+  title: "AftaStore | Afta",
+  description: "E-commerce website built with Next.js and Tailwind CSS",
 };
 
 export default function HomePage() {
