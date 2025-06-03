2f4e1ca413bf6e37bf172a5170af2b29580282b8
Afta20 <afreezap20@gmail.com>
2025-06-03 20:16:11 +0700

    ganti text

--- src/components/Home/Testimonials/testimonialsData.ts
+++ src/components/Home/Testimonials/testimonialsData.ts
@@ -2,44 +2,38 @@ import { Testimonial } from "@/types/testimonial";
 
 const testimonialsData: Testimonial[] = [
   {
-    review: `Lorem ipsum dolor sit amet, adipiscing elit. Donec
-    malesuada justo vitaeaugue suscipit beautiful vehicula`,
-    authorName: "Davis Dorwart",
+    review: `Fast delivery and top-notch quality! The wireless headphones I bought work perfectly and the sound is amazing.`,
+    authorName: "Jordi Venzen",
     authorImg: "/images/users/user-01.jpg",
-    authorRole: "Serial Entrepreneur",
+    authorRole: "Head Of Design Atma Luhur",
   },
   {
-    review: `Lorem ipsum dolor sit amet, adipiscing elit. Donec
-    malesuada justo vitaeaugue suscipit beautiful vehicula`,
-    authorName: "Wilson Dias",
+    review: `Great value for money. I purchased a laptop here and it exceeded my expectations in both performance and battery life.`,
+    authorName: "Yohanes",
     authorImg: "/images/users/user-02.jpg",
-    authorRole: "Backend Developer",
+    authorRole: "Himatika Web Developer",
   },
   {
-    review: `Lorem ipsum dolor sit amet, adipiscing elit. Donec
-    malesuada justo vitaeaugue suscipit beautiful vehicula`,
-    authorName: "Miracle Exterm",
+    review: `Excellent customer service! They helped me choose the best smartphone for my needs. Totally satisfied!`,
+    authorName: "NihanHan",
     authorImg: "/images/users/user-03.jpg",
-    authorRole: "Serial Entrepreneur",
+    authorRole: "Laravel enjoyer",
   },
   {
-    review: `Lorem ipsum dolor sit amet, adipiscing elit. Donec
-    malesuada justo vitaeaugue suscipit beautiful vehicula`,
-    authorName: "Thomas Frank",
+    review: `I love the sleek design and noise-cancellation of the headset I ordered. Shopping here was super easy.`,
+    authorName: "Tristan Thomas",
     authorImg: "/images/users/user-01.jpg",
     authorRole: "Entrepreneur",
   },
   {
-    review: `Lorem ipsum dolor sit amet, adipiscing elit. Donec
-    malesuada justo vitaeaugue suscipit beautiful vehicula`,
-    authorName: "Dave Smith",
+    review: `My new tablet arrived on time and was packed securely. Everything works perfectly. Will shop again!`,
+    authorName: "Angela Derossa",
     authorImg: "/images/users/user-02.jpg",
     authorRole: "Serial Entrepreneur",
   },
   {
-    review: `Lorem ipsum dolor sit amet, adipiscing elit. Donec
-    malesuada justo vitaeaugue suscipit beautiful vehicula`,
-    authorName: "Davis Dorwart",
+    review: `This store has become my go-to for electronics. Affordable, reliable, and always high quality.`,
+    authorName: "Griselda",
     authorImg: "/images/users/user-03.jpg",
     authorRole: "Serial Entrepreneur",
   },
