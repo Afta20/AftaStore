a2f18ae231c67bdeff51f44266557e270b9f5233
Afta20 <afreezap20@gmail.com>
2025-06-05 10:07:28 +0700

    data2

--- src/components/Shop/shopData.js
+++ src/components/Shop/shopData.js
@@ -5,50 +5,32 @@ const shopData = [
     price: 590000.0,
     discountedPrice: 290000.0,
     id: "cmbf17mqd0003egiwxj6vtdf4",
-    imgs: {
-      thumbnails: [
-        "/images/products/product-1-sm-1.png",
-        "/images/products/product-1-sm-2.png",
-      ],
-      previews: [
-        "/images/products/product-1-bg-1.png",
-        "/images/products/product-1-bg-2.png",
-      ],
+    imagePreviews: [
+      "/images/products/product-1-sm-1.png",
+      "/images/products/product-1-sm-2.png",
+    ],
     },
-  },
   {
     title: "iPhone 14 Plus , 6/128GB",
     reviews: 5,
     price: 8990000.0,
     discountedPrice: 6900000.0,
     id: "cmbf17msi0005egiwfhjmpe8y",
-    imgs: {
-      thumbnails: [
+    imagePreviews: [
         "/images/products/product-2-sm-1.png",
         "/images/products/product-2-sm-2.png",
       ],
-      previews: [
-        "/images/products/product-2-bg-1.png",
-        "/images/products/product-2-bg-2.png",
-      ],
-    },
-  },
+      },
   {
     title: "Apple iMac M1 24-inch 2021",
     reviews: 5,
     price: 5900000.0,
     discountedPrice: 2900000.0,
     id: "cmbf17mtd0007egiwy5vhkdty",
-    imgs: {
-      thumbnails: [
-        "/images/products/product-3-sm-1.png",
-        "/images/products/product-3-sm-2.png",
-      ],
-      previews: [
+    imgagePreviews: [
         "/images/products/product-3-bg-1.png",
         "/images/products/product-3-bg-2.png",
       ],
-    },
   },
   {
     title: "MacBook Air M1 chip, 8/256GB",
@@ -56,16 +38,10 @@ const shopData = [
     price: 5900000.0,
     discountedPrice: 2900000.0,
     id: "cmbf17mu80009egiwpyqty0uj",
-    imgs: {
-      thumbnails: [
-        "/images/products/product-4-sm-1.png",
-        "/images/products/product-4-sm-2.png",
-      ],
-      previews: [
+    imgagePreviews: [
         "/images/products/product-4-bg-1.png",
         "/images/products/product-4-bg-2.png",
       ],
-    },
   },
   {
     title: "Apple Watch Ultra",
@@ -73,16 +49,10 @@ const shopData = [
     price: 9900000.0,
     discountedPrice: 2900000.0,
     id: "cmbf17mv5000begiw9diy4vql",
-    imgs: {
-      thumbnails: [
-        "/images/products/product-5-sm-1.png",
-        "/images/products/product-5-sm-2.png",
-      ],
-      previews: [
+    imgagePreviews: [
         "/images/products/product-5-bg-1.png",
         "/images/products/product-5-bg-2.png",
       ],
-    },
   },
   {
     title: "Logitech MX Master 3 Mouse",
@@ -90,50 +60,32 @@ const shopData = [
     price: 590000.0,
     discountedPrice: 290000.0,
     id: "cmbf17mw2000degiwm2odzqdi",
-    imgs: {
-      thumbnails: [
-        "/images/products/product-6-sm-1.png",
-        "/images/products/product-6-sm-2.png",
-      ],
-      previews: [
+    imgagePreviews : [
         "/images/products/product-6-bg-1.png",
         "/images/products/product-6-bg-2.png",
       ],
     },
-  },
   {
     title: "Apple iPad Air 5th Gen - 64GB",
     reviews: 15,
     price: 5900000.0,
     discountedPrice: 2900000.0,
     id: "cmbf17mwy000fegiw8yp40qqu",
-    imgs: {
-      thumbnails: [
-        "/images/products/product-7-sm-1.png",
-        "/images/products/product-7-sm-2.png",
-      ],
-      previews: [
+    imgagePreviews :[
         "/images/products/product-7-bg-1.png",
         "/images/products/product-7-bg-2.png",
       ],
     },
-  },
   {
     title: "Asus RT Dual Band Router",
     reviews: 15,
     price: 590000.0,
     discountedPrice: 290000.0,
     id: "cmbf17mxu000hegiwa5653eph",
-    imgs: {
-      thumbnails: [
-        "/images/products/product-8-sm-1.png",
-        "/images/products/product-8-sm-2.png",
-      ],
-      previews: [
+    imgagePreviews :[
         "/images/products/product-8-bg-1.png",
         "/images/products/product-8-bg-2.png",
       ],
     },
-  },
 ];
 export default shopData;
