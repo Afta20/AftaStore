35135feb0b3a432df94c7f82cfaa669f4a953bf4
Afta20 <afreezap20@gmail.com>
2025-06-20 21:26:46 +0700

    change

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const nextConfig = {images: {
         protocol: 'https',
         hostname: 'placehold.co',
       },
+      {
+        protocol: 'https',
+        // --- PERHATIAN ---
+        // Ganti hostname ini dengan hostname dari URL Vercel Blob Anda.
+        // Anda bisa melihatnya saat meng-upload gambar atau dari URL gambar yang sudah ada.
+        // Biasanya formatnya adalah <random-string>.public.blob.vercel-storage.com
+        hostname: 'https%3A%2F%2F7q1vcc4wq46yhy6g.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
